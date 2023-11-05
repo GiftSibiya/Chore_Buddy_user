e068c0e77f99c48e32e525282f49f90377cfd03c
Gift Sibiya <144510689+GiftSibiya@users.noreply.github.com>
2023-11-05 21:27:05 +0200

    Create intro.dart
    created intro screen file

---
+++ intro.dart
@@ -0,0 +1,12 @@
+import 'package:donuts/constants/sizes.dart';
+
+class Intro extends StatelessWidget {
+  const Intro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //----- START POINT -----//
+    return 
+      child: Scaffold()
+    }
+}
