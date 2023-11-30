c63417829badc6401befb5810eee405151109d38
Gift Sibiya <144510689+GiftSibiya@users.noreply.github.com>
2023-11-30 11:38:53 +0200

    Update intro.dart
    removing useless files

--- intro.dart
+++ intro.dart
@@ -1,12 +1,1 @@
-import 'package:donuts/constants/sizes.dart';
 
-class Intro extends StatelessWidget {
-  const Intro({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    //----- START POINT -----//
-    return 
-      child: Scaffold()
-    }
-}
